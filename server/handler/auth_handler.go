efa2d5992dfc82b1b6e98d42739e43a4e5b87f3d
dandimuzaki <dandimuzaki@gmail.com>
2025-11-26 05:21:13 +0700

    fix: Memperbaiki cookie setting

--- server/handler/auth_handler.go
+++ server/handler/auth_handler.go
@@ -97,15 +97,19 @@ func Login(c *gin.Context) {
 	secure := isProd
 	serverHost := os.Getenv("SERVER_HOST")
 
-	c.SetCookie(
-    "token",
-    token,
-    3600*24,
-    "/",
-    serverHost,
-    secure,
-    true,
-	)
+	cookie := http.Cookie{
+		Name:     "token",
+		Value:    token,
+		Path:     "/",
+		Domain:   "badminton-app-production.up.railway.app",
+		MaxAge:   3600 * 24,
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteNoneMode,
+  }
+
+  http.SetCookie(c.Writer, &cookie)
+	
 	log.Printf(serverHost, secure)
 	c.JSON(http.StatusOK, gin.H{"message": "Login successful", "user": safeUser, "token": token})
 }
