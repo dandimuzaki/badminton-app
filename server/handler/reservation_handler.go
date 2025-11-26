a842f947c9224c9d1da31642f4e4449fd3675be8
dandimuzaki <dandimuzaki@gmail.com>
2025-11-26 22:02:23 +0700

    fix: Memperbaiki reservation handler

--- server/handler/reservation_handler.go
+++ server/handler/reservation_handler.go
@@ -72,7 +72,7 @@ func GetUserReservations(c *gin.Context) {
 
 	var reservations []model.Reservation
 	if err := initializers.DB.
-		Where("UserID = ?", userID).
+		Where("user_id = ?", userID).
 		Preload("User").
 		Preload("Court").
 		Preload("Timeslot").
